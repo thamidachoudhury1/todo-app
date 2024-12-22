bece1868349d003cf922d3bd4430c5a89d4832b6
thamidachoudhury1 <106630354+thamidachoudhury1@users.noreply.github.com>
2024-12-22 12:58:05 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,5 +18,5 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error(err));
 
 // Start server
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
